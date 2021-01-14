198758a338a4c2c965d875279a2c654b64f0bdbd
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-01-13 23:25:53 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anya";
+            int age = 18;
+            bool pet = false;
+            float shoeSize = 8.5F;
+
+            Console.WriteLine("My name is {0}", name);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do you have a pet? {0}", pet);
+            Console.WriteLine("My shoe size is {0}", shoeSize);
+
+        }
+    }
+}
