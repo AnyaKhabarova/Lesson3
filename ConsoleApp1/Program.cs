5bb4b4ce4e15b2c50432ea7c02c0995b251f9fdd
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-01-27 17:38:21 -0800

    Questionnaire

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,16 +5,17 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-            string name = "Anya";
-            int age = 18;
-            bool pet = false;
-            float shoeSize = 8.5F;
-
-            Console.WriteLine("My name is {0}", name);
-            Console.WriteLine("My age is {0}", age);
-            Console.WriteLine("Do you have a pet? {0}", pet);
-            Console.WriteLine("My shoe size is {0}", shoeSize);
+        {         
+            
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            var age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is {0}", birthdate);
+            
 
         }
     }
